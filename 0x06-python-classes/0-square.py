8fae3abdb999221d089ce5545a501ddc995e9bfe
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-03-29 15:44:49 -0700

    OOP

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This defines a square"""
+
+
+class Square:
+    """Represents a square with an empty class"""
+    pass
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Represents a Square class
+    Private instance attribute: size
+    Instantiation with optional size
+"""
+
+
+class Square:
+    """Represents a Square class"""
+    def __init__(self, size=0):
+        """Initialize the square with private instance attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates the current square area"""
+        return self.__size * self.__size
+
