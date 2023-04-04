c61fe93223d42841a02da7b0e8219cb6d4654933
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-03 19:03:33 -0700

    more

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,15 +12,6 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    def conditionChecker(self, value, attribute_name):
-        """Checks for the conditions for width and height:
-        If value is not an int, raise a TypeError
-        also, if value is less than 0, raise ValueError"""
-        if type(value) is not int:
-            raise TypeError(f"{attribute_name} must be an integer")
-        if value < 0:
-            raise ValueError(f"{attribute_name} must be >= 0")
-
     @property
     def width(self):
         """retrieves width"""
@@ -28,8 +19,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """sets the value ot width"""
-        self.conditionChecker(value, "width")
+        """sets the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -39,5 +33,8 @@ class Rectangle:
 
     @height.setter(self, value):
         "sets the value of height"
-        self.conditionChecker(value, "height")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError
         self.__height = value
