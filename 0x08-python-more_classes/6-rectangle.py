da4917b10251408195c8567df0c09122ba057702
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-05 04:52:16 -0700

    Task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,10 +8,13 @@
 class Rectangle:
     """defines a regular rectangle"""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """initializes the rectangle with width and height"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
