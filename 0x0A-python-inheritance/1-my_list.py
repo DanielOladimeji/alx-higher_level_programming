a6877e0a7f2d3951479779ef3a155a7ddb8c30e3
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-13 09:40:28 -0700

    One

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,7 @@ A class MyList that inherits from list """
 
 
 class MyList(list):
-
     """MyList, inherits from list"""
     def print_sorted(self):
         """prints the list sorted in ascending order"""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
