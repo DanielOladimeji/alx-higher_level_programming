cc2d23cf4f5ddaaf6b5d703298ae22f57ae7c2b6
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-13 16:21:48 -0700

    Task completed

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,18 @@
-11-square.py
+#!/usr/bin/python3
+"""Module that defines a rectangle subclass"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializes a new square instance
+        Args:
+            size (int): The size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
