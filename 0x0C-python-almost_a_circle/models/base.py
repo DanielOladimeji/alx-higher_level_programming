d1059426e5039bcccf23313e101e1d08d8c17ac2
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-19 04:16:27 -0700

    Task Completed

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" models base """
+
+
+class Base:
+    """ Defines a Model Base Clads"""
+    def __init__(self, id=None):
+        """ initializes a new base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
