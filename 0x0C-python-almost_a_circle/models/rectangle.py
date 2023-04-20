46bb07499a87262ef938c8a47a74e12939a968e3
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-20 07:39:32 -0700

    Task Done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,6 +94,7 @@ class Rectangle(Base):
             print(" " * self.x + "#" * self.width)
 
     def __str__(self):
-    return f"[Rectangle] ({self.id}){self.x}/{self.y} - \
+        """return the formatted str() when print()"""
+        return f"[Rectangle] ({self.id}){self.x}/{self.y} - \
 {self.width}/{self.height}"
 
