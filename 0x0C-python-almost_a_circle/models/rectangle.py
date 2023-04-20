335a2151cccd317d697509e7899e8dec059dff70
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-20 05:44:31 -0700

    Task Done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,10 +16,10 @@ class Rectangle(Base):
         NB: All arguments are int values
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
