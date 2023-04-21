a248e464b24bc3eb40ac500fa3da0378a2c8dd9f
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-21 11:45:23 -0700

    Task Done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,3 +33,29 @@ class Square(Rectangle):
         """ Returns a string representation of the Square """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
+
+    def update(self, *args, **kwargs):
+        """Update the rectangle instance with given arguments"""
+        if args is not None and len(args) != 0:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for argument in kwargs:
+                if "id" in kwargs:
+                    self.id = kwargs["id"]
+                if "width" in kwargs:
+                    self.width = kwargs["width"]
+                if "height" in kwargs:
+                    self.height = kwargs["height"]
+                if "x" in kwargs:
+                    self.x = kwargs["x"]
+                if "y" in kwargs:
+                    self.y = kwargs["y"]
