4e7ff5c64b4bb277d3ab94044f643aa4a4d80669
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-21 12:01:15 -0700

    Task Done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -49,7 +49,7 @@ class Square(Rectangle):
             for argument in kwargs:
                 if "id" in kwargs:
                     self.id = kwargs["id"]
-                if "width" in kwargs:
+                if "size" in kwargs:
                     self.size = kwargs["size"]
                 if "x" in kwargs:
                     self.x = kwargs["x"]
