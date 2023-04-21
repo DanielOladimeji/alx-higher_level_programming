eddeab9be710f7290d2d126cc7dbffed61857fdd
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-20 19:36:24 -0700

    Task Done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,15 +97,28 @@ class Rectangle(Base):
                                                        self.x, self.y,
                                                        self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update the rectangle instance with given arguments"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+        if args is not None and len(args) != 0:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for argument in kwargs:
+                if "id" in kwargs:
+                    self.id = kwargs["id"]
+                if "width" in kwargs:
+                    self.width = kwargs["width"]
+                if "height" in kwargs:
+                    self.height = kwargs["height"]
+                if "x" in kwargs:
+                    self.x = kwargs["x"]
+                if "y" in kwargs:
+                    self.y = kwargs["y"]
