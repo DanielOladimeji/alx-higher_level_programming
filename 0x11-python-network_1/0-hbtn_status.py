05529ee49d9e35daf1597e15da9bece491b29100
DanielOladimeji <danieloladimejiblessing@gmail.com>
2023-07-06 09:54:52 -0700

    done

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script fetches https://alx-intranet.hbtn.io/status"""
+
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    data = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(data)))
+print("\t- content: {}".format(data))
+print("\t- utf8 content: {data.decode('utf-8')}")
